07152aacdb4b9275bc2b93fefa93c37074609631
Bea Lugtu <lugtubealyssa@gmail.com>
2023-11-09 12:12:26 +0800

    User Interface Drop

---
+++ src/eduhub/GetStartedWIndowController.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package eduhub;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author lugtu
+ */
+public class GetStartedWIndowController implements Initializable {
+
+    private double x = 0;
+    private double y = 0;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void getStarted(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("selectRoleWindow.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+            stage.setWidth(916);
+            stage.setHeight(516);
+
+            Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+            double centerX = screenBounds.getMinX() + screenBounds.getWidth() / 2.0;
+            double centerY = screenBounds.getMinY() + screenBounds.getHeight() / 2.0;
+            stage.setX(centerX - 458);
+            stage.setY(centerY - 258);
+
+            Scene scene = new Scene(root, 916, 516);
+
+            stage.setScene(scene);
+            stage.show();
+
+            root.setOnMousePressed((mouseEvent) -> {
+                x = mouseEvent.getSceneX();
+                y = mouseEvent.getSceneY();
+            });
+
+            root.setOnMouseDragged((mouseEvent) -> {
+                stage.setX(mouseEvent.getScreenX() - x);
+                stage.setY(mouseEvent.getScreenY() - y);
+
+                stage.setOpacity(.8);
+            });
+
+            root.setOnMouseReleased((mouseEvent) -> {
+                stage.setOpacity(1);
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
