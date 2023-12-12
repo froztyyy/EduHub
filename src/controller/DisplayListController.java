c7502ec5e5788c9b52514b6cf251ba921a449a9c
Je <jcarl@zerofuku>
2023-12-13 03:06:03 +0800

    added completed task on student and fixed completed task table on officer

--- src/controller/DisplayListController.java
+++ src/controller/DisplayListController.java
@@ -22,8 +22,6 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
